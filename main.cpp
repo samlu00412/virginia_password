32fc5e89b4dcd998cf428d32fef68efb9b184336
samlu00412 <samlu00412@gmail.com>
2024-03-08 00:40:37 +0800

    建構程式框架

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+#define good ios_base::sync_with_stdio(0);cin.tie(0)
+typedef long long LL;
+using namespace std;
+string encryption(){
+
+}
+
+int main(){
+    string origin,key;
+    char mode;
+    cout << "Please select the mode. (E for encryption, and D for decryption.)";
+    
+    return 0;
+}

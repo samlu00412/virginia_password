28fad4c0c4f59df2e60d904d88262cce28bf316b
samlu00412 <samlu00412@gmail.com>
2024-03-09 23:12:46 +0800

    新增模式選擇、檢查密鑰格式

--- main.cpp
+++ main.cpp
@@ -3,14 +3,62 @@
 #define good ios_base::sync_with_stdio(0);cin.tie(0)
 typedef long long LL;
 using namespace std;
-string encryption(){
+
+string test(LL goout,string sentence);
+bool checkspace(string s);
+
+string encryption(string input,string keyword){
+    LL i;
 
 }
+string decryption(string input,string keyword){
 
+}
+bool checkspace(string s){
+    LL count;
+    for(count = 0; count < s.size();count++){
+        if(s[count] == ' ')
+            break;
+    }
+    return (count != s.size());
+}
+string test(LL goout,string sentence){
+    while(!(goout)){
+        if(checkspace(sentence)){
+            cout << "Wrong format! Please reinput the key." << endl;
+            cin >> sentence;
+        }
+        else
+            goout = 1;
+    }
+    return sentence;
+}
 int main(){
     string origin,key;
+    LL checkmode = 0,checkkey = 0;
+    LL i;
     char mode;
-    cout << "Please select the mode. (E for encryption, and D for decryption.)";
+    cout << "Please select the mode. (E for encryption, and D for decryption.)" << endl;
     
+    while (!(checkmode)){
+        if(mode == 'E'){
+            checkmode = 1;
+            cout << "Please type a sentence you want to encrypt." << endl;
+            cin >> origin;
+            cout << "Please input your key to encrypt the sentence." << endl;
+            cin >> key;
+            test(0,key);
+            
+        }
+        else if(mode == 'D'){
+            checkmode = 1;
+            cout << "Please type a sentence you want to decrypt." << endl;
+            cin >> origin;
+            cout << "Please input the key to decrypt the sentence." << endl;
+            cin >> key;
+        }
+        else
+            cout<< "Wrong format! Please select the mode. (E for encryption, and D for decryption.)" << endl;
+    }
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+#define good ios_base::sync_with_stdio(0);cin.tie(0)
+typedef long long LL;
+using namespace std;
+
+int main(){
+    LL i;
+    string s;
+    getline(cin,s);
+    cout << s.size();\
+    return 0;
+}

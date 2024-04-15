379b88982389b1dd5f03d81be16f82f00408ca74
samlu00412 <samlu00412@gmail.com>
2024-04-15 11:38:58 +0800

    add restart function

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
-#define good ios_base::sync_with_stdio(0);cin.tie(0)
-
 typedef long long LL;
+
 using namespace std;
 
 void encryption(string& input,string& keyword){
     LL i,skip = 0;
     string ciphertext;
     char cipher,key_char;
-    cin.ignore();
-    cout.flush();
     cout << "The cipher text is:";
 
     for(i = 0; i < input.size(); i++){
@@ -20,7 +17,7 @@ void encryption(string& input,string& keyword){
         }
         else{
             skip++;
-            ciphertext.push_back(' ');
+            ciphertext.push_back(input[i]);
         }
     }
 
@@ -30,8 +27,6 @@ void decryption(string& input,string& keyword){
     LL i,skip = 0;
     string plaintext;
     char plain,key_char;
-    cin.ignore();
-    cout.flush();
     cout << "The plain text is:";
 
     for(i = 0; i < input.size(); i++){
@@ -42,7 +37,7 @@ void decryption(string& input,string& keyword){
         }
         else{
             skip++;
-            plaintext.push_back(' ');
+            plaintext.push_back(input[i]);
         }
     }
 
@@ -51,34 +46,34 @@ void decryption(string& input,string& keyword){
 
 int main(){
     string origin,key;
-    LL checkmode = 0;
-    LL i;
     char mode;
-    cout << "Please select the mode. (e for encryption, and d for decryption.)\n";
+    cout << "Please select the mode. (e for encryption, d for decryption, or b for exit)\n";
     cin >> mode;
-    while (!(checkmode)){
+    while (mode != 'b'){
         if(mode == 'e'){
-            checkmode = 1;
             cout << "Please type a sentence you want to encrypt.\n";
             cin.ignore();
             getline(cin,origin);
             cout << "Please input your key to encrypt the sentence.(space is banned)\n";
             cin >> key;
-            cout.flush();
             encryption(origin,key);
+            cout << "Input b for exit, or select the mode. (e for encryption, or d for decryption.)\n";
+            cout.flush();
+            cin >> mode;
         }
         else if(mode == 'd'){
-            checkmode = 1;
             cout << "Please type a sentence you want to decrypt.\n";
             cin.ignore();
             getline(cin,origin);
             cout << "Please input the key to decrypt the sentence.(space is banned)\n";
             cin >> key;
-            cout.flush();
             decryption(origin,key);
+            cout << "Input b for exit, or select the mode. (e for encryption, or d for decryption.)\n";
+            cout.flush();
+            cin >> mode;
         }
         else{
-            cout<< "Wrong format! Please select the mode. (E for encryption, and D for decryption.)\n";
+            cout<< "Wrong format! Please select the mode. (e for encryption, d for decryption, or b for exit)\n";
             cin >> mode;
         }
     }
